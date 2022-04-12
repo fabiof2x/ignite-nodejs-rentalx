e2aa5b64c8a7ec03bccac4428b0f13a0f03806a5
fabiof2x <fabioar@gmail.com>
2022-04-12 17:29:29 -0300

    Conhecendo o conceito de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -6,7 +6,8 @@ class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
   handle(request: Request, response: Response): Response {
     const { file } = request;
-    console.log(file);
+
+    this.importCategoryUseCase.execute(file);
 
     return response.send();
   }
